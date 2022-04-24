ad8c4a2246a0705c93ed5f70275bd68c249a2757
Trevor Peitzman <trevor.peitzman@gmail.com>
2022-04-24 15:13:05 +0200

    remove metadata

--- budapest/assets/img/remove_metadata.sh
+++ budapest/assets/img/remove_metadata.sh
@@ -1,1 +1,9 @@
-exiftool -overwrite_original -all= *
+#!/bin/bash
+# IDK why this doesn't work lol
+FILES="$PWD/*"
+for file in $FILES
+do exiftool -overwrite_original -all= "$file"
+done;
+
+# really just run 
+# exiftool -overwrite_original -all= *
